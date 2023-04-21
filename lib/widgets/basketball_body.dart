d6934c02412178e9dd4af09abac4bad6fba08f53
Abdelrhman Abouelenan <96888940+abdoaboelaneen@users.noreply.github.com>
2023-04-21 23:04:26 +0200

    Add files via upload

---
+++ lib/widgets/basketball_body.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'team_standard.dart';
+
+class BasketballBody extends StatefulWidget {
+  const BasketballBody({Key? key}) : super(key: key);
+
+  @override
+  _BasketballBodyState createState() => _BasketballBodyState();
+}
+
+class _BasketballBodyState extends State<BasketballBody> {
+  int scoreA = 0;
+  int scoreB = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: TeamStandard(
+                  team: 'TEAM A',
+                  score: scoreA,
+                )),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: TeamStandard(
+                  team: 'TEAM B',
+                  score: scoreB,
+                )),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Center(
+              child: MaterialButton(
+                color: Color(0xff18892a),
+                onPressed: () {
+                  setState(() {
+                    scoreA = 0;
+                    scoreB = 0;
+                  });
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'RESET',
+                    style: TextStyle(
+                      fontSize: 26,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
